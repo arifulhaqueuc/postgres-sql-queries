e9d3c7f8ef94c23600cc5102506e2c30f15d0903
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-01 15:55:31 -0500

    Create script10.sql

---
+++ scripts/script10.sql
@@ -0,0 +1,7 @@
+
+--Write a SQL statement to find the highest purchase amount ordered by the each customer on a particular date with their ID, order date and highest purchase amount.
+
+
+SELECT customer_id,ord_date,MAX(purch_amt) 
+FROM orders 
+GROUP BY customer_id,ord_date;
