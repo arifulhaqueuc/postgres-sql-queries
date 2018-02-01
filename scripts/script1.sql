eb291de1e54a09fae519a0df418ca0674b43c31f
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-01 10:05:06 -0500

    Remove queries from data insertion script

--- scripts/script1.sql
+++ scripts/script1.sql
@@ -132,27 +132,3 @@ values
 
 ;
 
-------- QUERIES -------------------
-select *
-from president
-;
-
-select *
-from population
-;
-
-select *
-from languages
-;
-
-
-select 
-  president_table.country_name, 
-  president_table.president_name, 
-  president_table.job_started, 
-  population_table.pop_size, 
-  population_table.gdp
-from president as president_table
-inner join population as population_table
-on president_table.country_name =  population_table.country_name
-;
