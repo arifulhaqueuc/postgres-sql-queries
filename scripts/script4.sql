56ec89f891aeef66ebc39471ed0e983d4b4b51fc
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-02 10:58:00 -0500

    Update script4.sql

--- scripts/script4.sql
+++ scripts/script4.sql
@@ -1,4 +1,16 @@
 -- This is inner join
+-- general syntax
+
+select 
+  table_1.col_1,
+  table_1.col_2,
+  table_2.col_1
+from table_1
+inner join table_2
+on table_1.col_1 = table_2.col_2  
+;
+ 
+
 
 -- inner join----
 select 
