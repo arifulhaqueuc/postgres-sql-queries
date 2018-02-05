9996a4ed2f1b6c235490593b55cdf56879c08d64
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-05 15:33:18 -0500

    Update script13.sql

--- scripts/script13.sql
+++ scripts/script13.sql
@@ -1,9 +1,19 @@
 -- show running queries
 
-select query, pid, elapsed, substring from svl_qlog
---where userid = 100
+--option 1
+-- show all queries
+select query, pid, elapsed, substring 
+from svl_qlog
 order by starttime desc
-limit 5
+limit 10
 ;
 
 
+
+--option2 
+-- filter by userID
+select query, pid, elapsed, substring 
+from svl_qlog
+where userid = xxx
+order by starttime desc
+;
