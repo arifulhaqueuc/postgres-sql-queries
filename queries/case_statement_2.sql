176e6f381cadec41c12872a5b94db4e951de987e
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-28 14:58:15 -0500

    Create case_statement_2.sql

---
+++ queries/case_statement_2.sql
@@ -0,0 +1,33 @@
+
+
+
+SELECT player_name,
+       weight,
+       CASE WHEN weight > 250 THEN 'over 250'
+            WHEN weight > 200 THEN '201-250'
+            WHEN weight > 175 THEN '176-200'
+            ELSE '175 or under' END AS weight_group
+  FROM benn.college_football_players
+  
+  
+  
+  
+  SELECT player_name,
+       weight,
+       CASE WHEN weight > 250 THEN 'over 250'
+            WHEN weight > 200 AND weight <= 250 THEN '201-250'
+            WHEN weight > 175 AND weight <= 200 THEN '176-200'
+            ELSE '175 or under' END AS weight_group
+  FROM benn.college_football_players
+  
+  
+  
+  
+  SELECT player_name,
+       CASE WHEN year = 'FR' AND position = 'WR' THEN 'frosh_wr'
+            ELSE NULL END AS sample_case_statement
+  FROM benn.college_football_players
+  
+  
+  
+  
