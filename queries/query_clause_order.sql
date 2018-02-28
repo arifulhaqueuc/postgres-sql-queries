6fe7e10b4d331923d8534bd78c5dd5787f9ed572
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-28 15:33:48 -0500

    Create query_clause_order.sql

---
+++ queries/query_clause_order.sql
@@ -0,0 +1,14 @@
+
+
+
+Query clause order
+As mentioned in prior lessons, the order in which you write the clauses is important. Here’s the order for everything you’ve learned so far:
+
+SELECT
+FROM
+WHERE
+GROUP BY
+HAVING
+ORDER BY
+
+
