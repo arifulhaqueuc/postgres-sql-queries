3d35e5f9bf0ab6b12d2f8a49d06f6944285246f9
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-28 15:32:59 -0500

    Create having.sql

---
+++ queries/having.sql
@@ -0,0 +1,13 @@
+
+
+
+SELECT year,
+       month,
+       MAX(high) AS month_high
+  FROM tutorial.aapl_historical_stock_price
+ GROUP BY year, month
+HAVING MAX(high) > 400
+ ORDER BY year, month
+ 
+ 
+ 
