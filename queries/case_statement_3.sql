5d6654aaa83684a94337d428bc2bb159ee0850eb
Ariful Haque <arifulhaque2010@yahoo.com>
2018-02-28 15:10:40 -0500

    Create case_statement_3.sql

---
+++ queries/case_statement_3.sql
@@ -0,0 +1,25 @@
+
+General Syntax
+
+[option 1]
+CASE expression
+
+   WHEN value_1 THEN result_1
+   WHEN value_2 THEN result_2
+   WHEN value_n THEN result_n
+
+   ELSE result
+
+END
+
+
+[option 2]
+CASE
+
+   WHEN condition_1 THEN result_1
+   WHEN condition_2 THEN result_2
+   WHEN condition_n THEN result_n
+
+   ELSE result
+
+END
