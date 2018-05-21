71486052d1e7d39b3b002222ceb6e20b0b9a2a35
Ariful Haque <arifulhaque2010@yahoo.com>
2018-05-20 22:57:37 -0400

    Update readme

--- sql_queries/exists.sql
+++ sql_queries/exists.sql
@@ -1,12 +1,27 @@
-select dateid from date
+
+
+drop table if exists order_details
+;
+
+
+create temp table order_details
+(
+	order_date date,
+	dateid int(10)
+)
+;
+
+
+
+select 
+	dateid 
+
+from date
+
 where exists (
-select 1 from sales
-where date.dateid = sales.dateid
+	select 1 
+	from sales
+	where date.dateid = sales.dateid
 )
 order by dateid;
 
-dateid
---------
-1827
-1828
-1829
